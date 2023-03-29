fda07fdcb9e6ae28f1fec6f8e92937e056bd7268
Esteban Enriquez <114319655+esteban-94@users.noreply.github.com>
2023-03-28 19:25:40 -0500

    Update ejercicio_23_marzo.ts

--- ejercicio_23_marzo.ts
+++ ejercicio_23_marzo.ts
@@ -1,5 +1,12 @@
 // Ejercicio 23 Marzo
-
+//* Encuentre la nota mas alta de un estudiante
+// * Encuentre la nota mas alta de un curso.
+// * Valide si un estudiante tiene notas que tienen sentido
+// *  - Una nota tiene sentido si su valor está entre 0 y 5, su peso es mayor que 0% y menor o igual a 100%,
+// *      y la suma de todos los pesos no supera 100%
+// * Encuentre todos los estudiantes en riesgo de perder la materia.
+// *  - Un estudiante se considera en riesgo de perder la materia si ya tiene
+// *      más del 80% de las notas, y su promedio acumulado es menor o igual a 3.5
 
 type Nota = {
     nota: number;
